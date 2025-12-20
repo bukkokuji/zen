4f8804a6a178ff44989842b9b1efa537ab20d9a9
bukkokuji <bukkokuji.zen@gmail.com>
2025-12-20 14:27:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ window.addEventListener("scroll", () => {
   );
 
   /* Déplacement vertical très léger */
-  const shift = Math.min(scrollY * 0.03, 18); // max 40px
+  const shift = Math.min(scrollY / 12, 40);
   document.documentElement.style.setProperty(
     "--kamon-shift",
     `${shift}px`
