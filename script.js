d4b108aad5737b4ba853fff2b64c6c9c28b3ae73
bukkokuji <bukkokuji.zen@gmail.com>
2026-05-19 10:36:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -69,15 +69,3 @@ window.addEventListener("load", () => {
     if (hero) hero.classList.add("loaded");
   }, 1500); //
 });
-
-
-
-  /* Effets vidéo */
-window.addEventListener("scroll", () => {
-  const shift = window.scrollY * 0.03;
-
-  document.documentElement.style.setProperty(
-    "--video-shift",
-    `${shift}px`
-  );
-});
